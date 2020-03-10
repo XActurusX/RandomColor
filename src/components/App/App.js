90c361de6ff65140dc0b69a1888ff849b33eb1f6
XActurusX <jshelley553@gmail.com>
2020-03-10 16:17:55 +0700

    First Commit

---
+++ src/components/App/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Changer from '../Changer/Changer';
+
+
+
+class App extends React.Component {
+
+    render(){
+
+      return (
+        <div className="app">
+          <Changer />
+        </div>
+      );
+
+
+    }
+
+
+}
+
+
+
+
+
+export default App;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+export class Button extends React.Component {
+    render(){
+        return (
+            <button
+                className={this.props.light ? 'light-button' : 'dark-button'}
+                onClick={this.props.onClick}>
+                 CLICK!
+            </button>
+
+        );
+
+    }
+
+}
+
+export default Button;
